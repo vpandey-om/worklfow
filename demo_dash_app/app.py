4f4ca42ca4a070e5812b8be27e7b7fba61ed2543
Vikash <vkpepfl@gmail.com>
2026-06-18 22:57:14 +0200

    Allow Dash app port override

--- demo_dash_app/app.py
+++ demo_dash_app/app.py
@@ -1574,4 +1574,6 @@ if os.environ.get("SURVOM_DEBUG_CALLBACKS") == "1":
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host="127.0.0.1", port=8056)
+    host = os.environ.get("SURVOM_DASH_HOST", "127.0.0.1")
+    port = int(os.environ.get("SURVOM_DASH_PORT", "8056"))
+    app.run(debug=False, host=host, port=port)
